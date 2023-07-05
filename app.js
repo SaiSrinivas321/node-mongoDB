e3ee67589e31e22dcfae5562ad7242528f7edca6
SaiSrinivas321 <srinivas.32194@gmail.com>
2023-07-05 21:23:37 +0530

    db setup

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require("express");
+const { connectToDb, getDb } = require("./db");
 
 //init app
 const app = express();
 
 //routes
-
 app.get("/books", (req, res) => {
   res.json({ msg: "welcome to api" });
 });
 
-app.listen(8000, () => {
-  console.log("app listening on port 8000");
+//DB connection
+let db;
+
+connectToDb((err) => {
+  if (!err) {
+    app.listen(8000, () => {
+      console.log("app listening on port 8000");
+    });
+    db = getDb();
+  }
 });
