1c5b6114c2a4741c75e14fe88470fc5254e37cac
SaiSrinivas321 <srinivas.32194@gmail.com>
2023-07-06 11:10:26 +0530

    dotenv

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 const { MongoClient } = require("mongodb");
+const dotenv = require("dotenv");
+dotenv.config();
 
 let dbConnection;
 
 module.exports = {
   connectToDb: (cb) => {
-    MongoClient.connect("mongodb://localhost:27017/bookstore")
+    MongoClient.connect(process.env.DATABASE_URI)
       .then((client) => {
         dbConnection = client.db();
         return cb();
