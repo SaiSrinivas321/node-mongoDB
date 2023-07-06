0419e2ec06ed6871198968547cd06223030ec801
SaiSrinivas321 <srinivas.32194@gmail.com>
2023-07-06 10:54:37 +0530

    CRUD Operations

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const { connectToDb, getDb } = require("./db");
+const { ObjectId } = require("mongodb");
 
 //init app
 const app = express();
 
-//routes
-app.get("/books", (req, res) => {
-  res.json({ msg: "welcome to api" });
-});
+//Middleware
+app.use(express.json());
 
 //DB connection
 let db;
@@ -18,5 +17,81 @@ connectToDb((err) => {
       console.log("app listening on port 8000");
     });
     db = getDb();
+  }
+});
+//routes
+app.get("/books", (_, res) => {
+  let books = [];
+  db.collection("books")
+    .find()
+    .sort({ author: 1 })
+    .forEach((book) => books.push(book))
+    .then(() => {
+      res.status(200).json(books);
+    })
+    .catch(() => {
+      res.status(500).json({ error: "Could not fetch the documents" });
+    });
+});
+
+app.get("/book/:id", (req, res) => {
+  let id = req.params.id;
+  if (ObjectId.isValid(id)) {
+    db.collection("books")
+      .findOne({ _id: new ObjectId(id) })
+      .then((doc) => {
+        res.status(200).json(doc);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: "Could not fetch the documents" });
+      });
+  } else {
+    res.status(500).json({ error: "Not a valid Id" });
+  }
+});
+
+app.post("/books", (req, res) => {
+  const book = req.body;
+
+  db.collection("books")
+    .insertOne(book)
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      res.status(500).json({ err: "Could not create a new document" });
+    });
+});
+
+app.delete("/book/:id", (req, res) => {
+  let id = req.params.id;
+  if (ObjectId.isValid(id)) {
+    db.collection("books")
+      .deleteOne({ _id: new ObjectId(id) })
+      .then((result) => {
+        res.status(200).json(result);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: "Could not delte the documents" });
+      });
+  } else {
+    res.status(500).json({ error: "Not a valid Id" });
+  }
+});
+
+app.patch("/book/:id", (req, res) => {
+  const updates = req.body;
+  let id = req.params.id;
+  if (ObjectId.isValid(id)) {
+    db.collection("books")
+      .updateOne({ _id: new ObjectId(id) }, { $set: updates })
+      .then((result) => {
+        res.status(200).json(result);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: "Could not update the documents" });
+      });
+  } else {
+    res.status(500).json({ error: "Not a valid Id" });
   }
 });
